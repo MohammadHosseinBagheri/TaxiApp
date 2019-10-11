5c3095c3c8db2a67f2af3f47b1b861d6db833a96
MohammadHosseinBagheri <mohammadbagheri228@gmail.com>
2019-10-11 16:52:27 -0700

    Add DeviceLocation

--- index.js
+++ index.js
@@ -7,4 +7,4 @@ import App from './App';
 import {name as appName} from './app.json';
 import Home from './src/page/home/Home';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => Home);
--- src/page/home/Home.js
+++ src/page/home/Home.js
@@ -1,40 +1,53 @@
-import React, { Component } from 'react';
-import { View, Text,StyleSheet } from 'react-native';
-import MapView , { PROVIDER_GOOGLE } from 'react-native-maps';
+import React, {Component} from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+import MapView, {Marker} from 'react-native-maps';
+import Geolocation from '@react-native-community/geolocation';
 const styles = StyleSheet.create({
   container: {
     ...StyleSheet.absoluteFillObject,
-    height: 400,
-    width: 400,
-    justifyContent: 'flex-end',
+    flex: 0.8,
     alignItems: 'center',
   },
   map: {
     ...StyleSheet.absoluteFillObject,
   },
- });
+});
 class Home extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      latitude: 0,
+      longitude: 0,
+      error: null,
     };
   }
-
+  componentDidMount() {
+    Geolocation.getCurrentPosition(
+      possition => {
+        this.setState({
+          latitude: possition.coords.latitude,
+          longitude: possition.coords.longitude,
+          error: null,
+        });
+      },
+      error => console.log(error),
+      {enableHighAccuracy: true, timeout: 20000},
+    );
+  }
   render() {
     return (
       <View style={styles.container}>
-     <MapView
-       provider={PROVIDER_GOOGLE} // remove if not using Google Maps
-       style={styles.map}
-       region={{
-         latitude: 37.78825,
-         longitude: -122.4324,
-         latitudeDelta: 0.015,
-         longitudeDelta: 0.0121,
-       }}
-     >
-     </MapView>
-   </View>
+        <MapView // remove if not using Google Maps
+          style={styles.map}
+          region={{
+            latitude: this.state.latitude,
+            longitude: this.state.longitude,
+            latitudeDelta: 0.015,
+            longitudeDelta: 0.0121,
+          }}>
+          <Marker coordinate={this.state} title={'your location'} description={this.state.latitude.toString()} />
+        </MapView>
+      </View>
     );
   }
 }
